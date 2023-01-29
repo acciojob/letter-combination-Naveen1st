d42211fdb67eea00b63c6d086dd07b9f7dc5cae9
Naveen1st <naveenbanoth4889@gmail.com>
2023-01-29 10:21:19 +0530

    third

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+"use strict";
 function letterCombinations(input_digit) {
-  if (!input_digit) return [];
-  let result = [''];
-  for (let input_digit of input_digit) {
-    const letters = mapping[input_digit];
-    const temp = [];
-    for (let letter of letters) {
-      for (let combination of result) {
-        temp.push(combination + letter);
-      }
-    }
-    result = temp;
-  }
-  return result.sort();
+  //Complete the function
+  let ans = [],
+    temp = "";
+  fun(input_digit, ans, temp);
+  return ans;
 }
 
+let map = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+
+let fun = (input, ans, temp) => {
+  if (input.length === 0) {
+    ans.push(temp);
+    return;
+  }
+
+  let a = input.charAt(0) - "0";
+  let b = input.substring(1);
+  let c = map[a];
+  for (let i = 0; i < c.length; i++) {
+    let z = c.charAt(i);
+    fun(b, ans, temp + z);
+  }
+};
+
+// console.log(letterCombinations("013"));
 module.exports = letterCombinations;
